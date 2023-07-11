209a6a6dfb7395ba5112b9f165ae935896d7bf91
thanhminhspk05 <thanhminhspk05@gmail.com>
2023-07-11 17:33:34 +0700

    lesson 290

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from './ui/Home';
+import Menu, { loader as menuLoader } from './features/menu/Menu';
+import Cart from './features/cart/Cart';
+import Order from './features/order/Order';
+import CreateOrder from './features/cart/CreateOrder';
+import AppLayout from './ui/AppLayout';
+import Error from './ui/Error';
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/menu', element: <Menu />, loader: menuLoader },
+      { path: '/cart', element: <Cart /> },
+      { path: '/oder/new', element: <CreateOrder /> },
+      { path: '/oder/:orderId', element: <Order /> },
+    ],
+  },
+]);
 
 function App() {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/features/order/SearchOrder.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function SearchOrder() {
+  const [query, setQuery] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(query);
+
+    if (query) return;
+    navigate(`/order`);
+    setQuery('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder="Search order #"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+    </form>
+  );
+}
+
+export default SearchOrder;
