f8be4d552dbe62bded908721b60a0ae5e3b6d8ec
thanhminhspk05 <69253365+thanhminhspk05@users.noreply.github.com>
2023-07-23 17:16:53 +0700

    lesson 297

---
+++ src/features/user/UserName.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function UserName() {
+  return <div className="text-sm font-semibold">Minh</div>;
+}
+
+export default UserName;
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import SearchOrder from '../features/order/SearchOrder';
+import UserName from '../features/user/UserName';
 
 function Header() {
   return (
-    <header>
-      <Link to="/">Fast React Pizza Co.</Link>
-
+    <header className="bg-yellow-500 uppercase">
+      <Link
+        to="/"
+        className="tracking-widest "
+      >
+        Fast React Pizza Co.
+      </Link>
       <SearchOrder />
+      <UserName />
     </header>
   );
 }
